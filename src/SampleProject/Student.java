297e4f5f3db57156bfb654c7d56d18459d755db4
Elshan Mamedov <elikmtl@gmail.com>
2024-02-09 20:16:32 -0500

    added fields

--- src/SampleProject/Student.java
+++ src/SampleProject/Student.java
@@ -3,5 +3,9 @@ package SampleProject;
 public class Student {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Toronto");
+        System.out.println("Hello Ontario");
+        System.out.println("Hello Penthouse");
+        System.out.println("Hello 50th floor");
     }
 }
